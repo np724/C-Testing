0830f5a8ce54aa47de9cf2f658e163b28ee83316
np724 <124282603+np724@users.noreply.github.com>
2024-02-05 18:15:52 -0500

    Update HWDynArray.cc

--- HWDynArray.cc
+++ HWDynArray.cc
@@ -4,36 +4,91 @@ private:
 	uint32_t size; // the number of elements used
 	uint32_t capacity; // the amount of memory
 	void checkGrow() {
-	  if (memory does not need to grow)
+	  if (size < capacity)
 	    return;
 	  
 		// if the memory needs to grow, double in size
 		//TODO: YOU IMPLEMENT THIS
+	capacity *= 2;
+        int* newP = new int[capacity];
+        for (uint32_t i = 0; i < size; i++) {
+            newP[i] = p[i];
+        }
+        delete[] p;
+        p = newP;
 	}
 public:
 	//write constructor, destructor
+	// Constructor
+    GrowArray(uint32_t initialCapacity) : size(0), capacity(initialCapacity) {
+        p = new int[capacity];
+    }
 
+    // Destructor
+    ~GrowArray() {
+        delete[] p;
+    }
 	//copy constructor + 10%
+GrowArray(const GrowArray& other) : size(other.size), capacity(other.capacity) {
+        p = new int[capacity];
+        for (uint32_t i = 0; i < size; i++) {
+            p[i] = other.p[i];
+        }
+    }
+
+    // Copy Assignment Operator
+    GrowArray& operator=(const GrowArray& other) {
+        if (this != &other) {
+            delete[] p;
+            size = other.size;
+            capacity = other.capacity;
+            p = new int[capacity];
+            for (uint32_t i = 0; i < size; i++) {
+                p[i] = other.p[i];
+            }
+        }
+        return *this;
+    }
 	//double ended?  + 20% (SEE circularbuffer)
 	void addEnd(int v) { //O(____________)
-		
-		
+		checkGrow();
+        	p[size++] = v;
 	}
 
 	void addStart(int v) { //O(____________)
-
+		checkGrow();
+        	for (uint32_t i = size; i > 0; i--) {
+            		p[i] = p[i - 1];
+        	}
+        	p[0] = v;
+        	size++;
 	}
 	void removeStart() { //O(____________)
-
+		if (size == 0)
+            		return;
+        	for (uint32_t i = 0; i < size - 1; i++) {
+            		p[i] = p[i + 1];
+        	}
+        s	ize--;
 	}
 	
 	void removeEnd() { //O(____________)
-
+		if (size == 0)
+            		return;
+        	size--;
 	}
 	uint32_t getSize() const { //O(____________)
 		return size;
 	}
-	
+	void removeEvens() {
+        	uint32_t newSize = 0;
+        	for (uint32_t i = 0; i < size; i++) {
+            		if (p[i] % 2 != 0) {
+                		p[newSize++] = p[i];
+            		}
+        	}
+        	size = newSize;
+    }
 };
 
 int main() {
